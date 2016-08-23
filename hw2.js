ec3ce4f3d7cac6fbb83e135a1be627eda3c4527f
ram4507 <ram4507@rambler.ru>
2016-08-23 20:05:48 +0200

    Add files via upload

---
+++ hw2.js
@@ -0,0 +1,14 @@
+var mass =[];
+for (var i = 0; i < 100; i++)
+{
+	if (i % 3==0 && i % 5==0) 
+		mass[i] = "FizzBuzz";
+
+	if (i % 3==0) 
+       mass[i] = "Fizz";
+
+	if (i % 5==0) 
+        mass[i] = "Buzz";
+	}
+//alert (mass);//
+consol.log(mas[i]);
