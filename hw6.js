72638370324b93a3cb4a04c0fe58f4dfa126ba3c
ram4507 <ram4507@rambler.ru>
2016-09-21 17:05:31 +0200

    Add files via upload

--- hw6.js
+++ hw6.js
@@ -84,3 +84,112 @@ var keys = ['name','second name','age','email','friends'];
 var person = [];
 pick(user, keys);
 console.log(person);
+
+/*практика*/
+var person = [
+],
+count_pers = 1,
+del_user = '';
+function addUser() {
+  var user = [
+  ];
+  user.id = count_pers;
+  user.firstName = prompt('Введите имя:');
+  user.lastName = prompt('Введите фамилию:');
+  user.birthDay = prompt('Дату рождения:');
+  phone = [
+  ];
+  var n = true,
+  i = 0;
+  while (n == true) {
+    phone[i] = prompt('Номер телефона:');
+    phone.push(phone[i]);
+    n = confirm('имеется еще телефон?');
+  };
+  phone.pop();
+  user.tlf = phone.toString();
+  for (var b = 0; b < user.length; ++b) {
+    user.push(user[b]);
+  }
+  user.join(',');
+  person.push(user);
+}
+function csvFormat() {
+  var persona = '';
+  for (i = 0; i < person.length; i++)
+  {
+    persona = persona + person[i].id + ';' + person[i].firstName + ';' + person[i].lastName + ';' + person[i].birthDay + ';' + person[i].tlf + ';' + '\n';
+  }
+  return persona;
+}
+function deluser(del_user) {
+  var id = '',
+  n = del_user;
+  /*n = parseInt(prompt('Укажите код пользователя ?'));*/
+  id = parseInt(person[n - 1].id);
+  i = 0
+  while (i !== person[n - 1].id) {
+    i++
+  }
+  if (i == person[n - 1].id)
+  {
+    person.splice(n - 1, 1);
+  }
+	else{ 
+		alert('пользователь с таким кодом не найден');}
+  return person[0].lastName;
+}
+
+/*поиск по имени,фамилии*/
+function search(value,person){
+  var nam='';
+	var re = new RegExp(value,'i');
+   for(var b=0; b<=person.length -1; b++){
+     if (С==1) { break;}
+     else 
+     person.forEach(function(value){
+     nam= person[b].firstName;
+      if(re.test(nam)){
+        nam= person[b].firstName + ' '+person[b].lastName;
+        C=1;
+        result.push(nam);
+        }
+       });
+  } 
+return result;
+}
+var add = confirm('Внимание!! Добавляем пользователя');
+while (add == true) {
+  addUser(count_pers);
+  count_pers++;
+  add = confirm('Будут еще пользователи?');
+}
+
+console.log(csvFormat());
+
+var del = confirm('Желаете удалить пользователя?');
+while (del == true) {
+  del_user = parseInt(prompt('Укажите код пользователя ?'));
+  if (del_user >= count_pers | del_user == 0) {
+    alert('Пользователя с кодом ' + 'нет');
+  } 
+  else {
+    deluser(del_user);
+  }
+  del = confirm('Будут еще удаляемые пользователи?');
+}
+console.log(csvFormat());
+
+var poisk = confirm('Поиск ?');
+var result=[], C=0;
+while (poisk == true){
+   poisk = prompt('Кого ищем? : ')
+  poisk = poisk.toLowerCase();
+    search(poisk,person);
+  if (C==0){
+    alert('Такого человека в базе нет');
+    poisk = confirm('Продолжим ?')}
+    else 
+      {console.log(result[0]);
+       poisk = confirm('Продолжим ?')
+       }}
